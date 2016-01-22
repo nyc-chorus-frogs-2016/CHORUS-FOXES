7bf78f2cccb8829b6ec0d3df88fd41c1d75b3fa5
Dan Andersen <dandersen2@gmail.com>
2016-01-22 15:20:14 -0500

    Add notes for work-flow

---
+++ Agile_Work-flow_notes.rb
@@ -0,0 +1,15 @@
+Agile Work-flow notes:
+
+1st SCRUM:
+____________________________________________
+Scott & Kerry --> sessions, user layout
+*sessions_controller.rb
+*user_controller.rb
+*layout.erb
+
+Mira & Dan --> Homepage(landing page), round page, decks controller
+views/decks/index.erb
+decks_controller.rb
+views/rounds/show.erb
+
+Standup at 4:15pm
--- app/models/round.rb
+++ app/models/round.rb
@@ -3,7 +3,15 @@ class Round < ActiveRecord::Base
   has_many :guesses
 
   belongs_to :deck
-
   belongs_to :user
 
+  #round.deck.cards.sample --> returns one random card
+
+  # round.guesses --> remove the cards where the card_id.correct? == true
+       # ==> lists the cards remaining to be guessed
+
+
+
 end
+
+
